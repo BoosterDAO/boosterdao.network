394078d6a30abbfcf271b1da25b713a1cc320360
equaliser0 <accounts@equaliser.minimalcity.de>
2022-01-10 18:50:21 +0100

    hotfix update

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b58170c22e617893b37e";
+/******/ 	var hotCurrentHash = "7ac90a0436511525ed9b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
