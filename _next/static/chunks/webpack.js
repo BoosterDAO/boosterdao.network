d6d77212138f96a035fd72d80d92e7c6c80479c6
equaliser0 <accounts@equaliser.minimalcity.de>
2022-01-19 15:42:09 +0100

    logo fix

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2e4d323209d825bc5ed7";
+/******/ 	var hotCurrentHash = "38ad3ff4008f15459599";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
